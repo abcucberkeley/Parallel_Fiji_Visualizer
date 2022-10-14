5ee3c49272661b4e91c4d369dd1118b90f9327aa
MattHazard <mrm.mue@gmail.com>
2022-10-14 15:48:49 -0700

    Added support on the java side for reading in a ropped zarr

--- Parallel_Fiji_Visualizer/src/main/java/edu/abc/berkeley/PRZ.java
+++ Parallel_Fiji_Visualizer/src/main/java/edu/abc/berkeley/PRZ.java
@@ -5,6 +5,7 @@ import java.io.File;
 import ij.IJ;
 import ij.ImagePlus;
 import ij.ImageStack;
+import ij.ParallelImagePlus;
 import ij.ParallelVirtualStack;
 import ij.VirtualStack;
 import ij.plugin.Memory;
@@ -14,11 +15,18 @@ public class PRZ {
 	private ImageStack stack;
 	private VirtualStack vStack;
 
-	public PRZ(String fileName, boolean showImage){
+	public PRZ(String fileName, long startX, long startY, long startZ, long endX, long endY, long endZ, boolean showImage){
 		PRZC przc = new PRZC();
 		long bits = przc.getDataType(fileName);
 		long[] dims = przc.getImageDims(fileName);
-
+		if(endX == -1) endX = dims[0];
+		if(endY == -1) endY = dims[1];
+		if(endZ == -1) endZ = dims[2];
+		
+		dims[0] = endX-startX;
+		dims[1] = endY-startY;
+		dims[2] = endZ-startZ;
+		
 		File f = new File(fileName);
 
 
@@ -37,26 +45,26 @@ public class PRZ {
 			stack = new ImageStack((int)dims[1],(int)dims[0]);
 			stack.setBitDepth(tBits);
 			if(bits == 8) {
-				byte im[][] = przc.parallelReadZarrUINT8(fileName,0,0,0,dims[0],dims[1],dims[2]);
+				byte im[][] = przc.parallelReadZarrUINT8(fileName,startX,startY,startZ,dims[0],dims[1],dims[2]);
 				for(int i = 0; i < dims[2]; i++){
 					stack.addSlice(null, im[i]);
 				}
 			}
 			else if (bits == 16) {
 				
-				short im[][] = przc.parallelReadZarrUINT16(fileName,0,0,0,dims[0],dims[1],dims[2]);
+				short im[][] = przc.parallelReadZarrUINT16(fileName,startX,startY,startZ,dims[0],dims[1],dims[2]);
 				for(int i = 0; i < dims[2]; i++){
 					stack.addSlice(null, im[i]);
 				}
 			}
 			else if (bits == 32) {
-				float im[][] = przc.parallelReadZarrFLOAT(fileName,0,0,0,dims[0],dims[1],dims[2]); 
+				float im[][] = przc.parallelReadZarrFLOAT(fileName,startX,startY,startZ,dims[0],dims[1],dims[2]); 
 				for(int i = 0; i < dims[2]; i++){
 					stack.addSlice(null, im[i]);
 				}
 			}
 			else if(bits == 64) {
-				float im[][] = przc.parallelReadZarrDOUBLE(fileName,0,0,0,dims[0],dims[1],dims[2]);
+				float im[][] = przc.parallelReadZarrDOUBLE(fileName,startX,startY,startZ,dims[0],dims[1],dims[2]);
 				for(int i = 0; i < dims[2]; i++){
 					stack.addSlice(null, im[i]);
 				}
@@ -73,6 +81,8 @@ public class PRZ {
 		else {
 			ParallelVirtualStack pVStack = new ParallelVirtualStack((int)dims[1],(int)dims[0],null,fileName);
 			pVStack.setBitDepth(tBits);
+			ParallelImagePlus imp = new ParallelImagePlus(f.getName(),pVStack);
+			imp.show();
 			/*
 			vStack = new VirtualStack((int)dims[1],(int)dims[0],null,fileName);
 			vStack.setBitDepth(tBits);
@@ -83,9 +93,15 @@ public class PRZ {
 
 		}
 	}
-
+	
+	// Open the entire image
+	public PRZ(String fileName, boolean showImage){
+		this(fileName, 0, 0, 0, -1, -1, -1, showImage);
+	}
+	
+	// Open the entire image and always show the image after
 	public PRZ(String fileName){
-		this(fileName,true);
+		this(fileName, 0, 0, 0, -1, -1, -1, true);
 	}
 
 	public ImagePlus getImp() {
