ee0912137b4c2d825ca5133e2552c32a2f5e694d
Matthew Mueller <mrm.mue@gmail.com>
2022-11-02 20:14:33 -0700

    Revert PWZ.java

--- Parallel_Fiji_Visualizer/src/main/java/edu/abc/berkeley/PWZ.java
+++ Parallel_Fiji_Visualizer/src/main/java/edu/abc/berkeley/PWZ.java
@@ -8,33 +8,18 @@ import ij.IJ;
 import ij.ImagePlus;
 import ij.ImageStack;
 
-import edu.abc.berkeley.PWZGUI;
-
-import edu.abc.berkeley.PWZGUI;
-
 public class PWZ {
-	PWZC pwzc;
-	JFileChooser chooser;
-	File f;
-
-	
 	PWZ(){
-		System.out.println("PWZ Constructor Here!");
-		new PWZGUI();
-		// PWZC pwzc = new PWZC();
-		// JFileChooser chooser = new JFileChooser();
-		// pwzc = new PWZC();
-		/*chooser = new JFileChooser();
-		f=null;
-
+		PWZC pwzc = new PWZC();
+		JFileChooser chooser = new JFileChooser();
 		chooser.setApproveButtonText("Save");
 		chooser.setDialogTitle("Save as Zarr");
-		// File f = null;
-
+		File f = null;
 		int returnValue = chooser.showOpenDialog(null);
-		if(returnValue == JFileChooser.APPROVE_OPTION) f = chooser.getSelectedFile();
+		if(returnValue == JFileChooser.APPROVE_OPTION) {
+			f = chooser.getSelectedFile() ;
+		}
 		if(f == null) return;
-		
 		ImagePlus cImagePlus = IJ.getImage();
 		//WindowManager.getCurrentImage();
 		
@@ -47,7 +32,7 @@ public class PWZ {
 		String fileName = f.getPath();
 		int x = cImageStack.getWidth();
 		int y = cImageStack.getHeight();
-		int z = cImageStack.getSize();*/
-		// pwzc.parallelWriteZarr(fileName, cImageObj, 0, 0, 0, y, x, z, 256, 256, 256, 1, "lz4", 1, bits);
+		int z = cImageStack.getSize();
+		pwzc.parallelWriteZarr(fileName, cImageObj, 0, 0, 0, y, x, z, 256, 256, 256, 1, "lz4", 1, bits);
 	}
 }
